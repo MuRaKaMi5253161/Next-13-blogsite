ea3cebf849d16fa0308146eacdf052fb3145ed92
MuRaKaMi5253161 <taitai7121@gmail.com>
2024-09-27 09:22:54 +0900

    api周りの実装をnextjs-13の仕様に置き換え

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -17,7 +17,7 @@ function CreateBlogPage() {
     // await createArticle(id, title, content);
 
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    await fetch(`${API_URL}/api/create`, {
+    await fetch(`${API_URL}/api/blog`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
